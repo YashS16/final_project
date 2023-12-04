8ff2f7bcc9035d9ca955be853561eb4b78697178
Yash Shrivastava <yashs1623@gmail.com>
2023-12-04 21:32:57 +0100

    Add files via upload

--- interface/App.py
+++ interface/App.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from keras.models import load_model
+# from keras.models import load_model
 from PIL import Image
 import numpy as np
 # from streamlit_option_menu import option_menu
