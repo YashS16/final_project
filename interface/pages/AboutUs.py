ea02e409a92ae38b02b10ae850320812382c1901
Yash Shrivastava <yashs1623@gmail.com>
2023-12-06 14:02:09 +0100

    Add files via upload

--- interface/pages/AboutUs.py
+++ interface/pages/AboutUs.py
@@ -5,79 +5,58 @@ import os
 # from streamlit_option_menu import option_menu
 
 
-# #Menu
-# selected = option_menu(None, ["Home", "Covid", "Brain", "About us" ],
-#                     icons=['house', 'lungs', "f5dc", "people"],
-#                     on_change=on_change, key='menu_4', orientation="horizontal")
+st.write("# More about SmartDiag Tech! 🔎")
 
-# if selected =="Home":
-#     #st.rerun() or st.experimental_rerun()
-#     print('Yes')
-# if selected == "Covid":
-#     subprocess.Popen(["streamlit", "run", "covid.py"])
-#     os._exit(0)
-# if selected == "Brain":
-#     MRI()
-# if selected == "About us":
-#     subprocess.Popen(["streamlit", "run", "aboutus.py"])
-#     os._exit(0)
+st.markdown(
+"""
+### Our Mission
+At SmartDiag, we leverage AI to enhance medical diagnostic capabilities.We are driven by a vision where healthcare is empowered by cutting-edge technology, making diagnostics more accurate, accessible, and efficient for everyone.
 
-def about():
-    st.write("# More about SmartDiag Tech! 🔎")
+### Who We Are
+Since our establishment in 2023, SmartDiag has been a pioneering force in the healthcare industry. With a commitment to innovation and excellence, we have consistently led the way in advancing diagnostic solutions through cutting-edge technology.
 
-    st.markdown(
-    """
-    ### Our Mission
-    At SmartDiag, we leverage AI to enhance medical diagnostic capabilities.We are driven by a vision where healthcare is empowered by cutting-edge technology, making diagnostics more accurate, accessible, and efficient for everyone.
+### Meet Our Team
+Our dedicated team brings a wealth of experience and skills to SmartDiag. Learn more about the individuals driving our mission.
+"""
+)
 
-    ### Who We Are
-    Since our establishment in 2023, SmartDiag has been a pioneering force in the healthcare industry. With a commitment to innovation and excellence, we have consistently led the way in advancing diagnostic solutions through cutting-edge technology.
+col1, col2, col3, col4 = st.columns(4)
 
-    ### Meet Our Team
-    Our dedicated team brings a wealth of experience and skills to SmartDiag. Learn more about the individuals driving our mission.
-    """
-    )
+with col1:
+    st.image(os.path.join(os.path.dirname(__file__),"..", "pages", "Yash.jpg"), width=170, caption='Yash Shrivastava')
 
-    col1, col2, col3, col4 = st.columns(4)
+with col2:
+    st.image(os.path.join(os.path.dirname(__file__),"..", "pages", "Faisal.jpg"), width=170, caption='Rahimdad Faisal Safi')
 
-    with col1:
-        st.image('os.path.join(os.path.dirname(__file__),"..", "pages", "Yash.jpg"', width=170, caption='Yash Shrivastava')
+with col3:
+    st.image(os.path.join(os.path.dirname(__file__),"..", "pages", "Sara.png"), width=170, caption='Sara Nourizadeh')
 
-    with col2:
-        st.image('os.path.join(os.path.dirname(__file__),"..", "pages", "Faisal.jpg"', width=170, caption='Rahimdad Faisal Safi')
+with col4:
+    st.image(os.path.join(os.path.dirname(__file__),"..", "pages", "Lore.jpg"), width=170, caption='Lorenzo Mosini')
 
-    with col3:
-        st.image('os.path.join(os.path.dirname(__file__),"..", "pages", "Sara.png"', width=170, caption='Sara Nourizadeh')
+st.markdown(
+"""
 
-    with col4:
-        st.image('os.path.join(os.path.dirname(__file__),"..", "pages", "Lore.jpg"', width=170, caption='Lorenzo Mosini')
+### Model Performance
+"We have implemented the VGG16 model as the cornerstone of our diagnostic approach, utilizing its robust capabilities in analyzing medical images.
+Trained on a dataset comprising 7000 chest X-ray images and 3500 Brain MRI images, our model has demonstrated commendable accuracy in predicting results.
 
-    st.markdown(
-    """
+### Ongoing Efforts and Future Goals
+"As we continually strive for excellence, our commitment to enhancing performance remains unwavering. We envision expanding our dataset in the near future, a crucial step towards refining our model's predictive accuracy.
+The pursuit of a larger and more diverse dataset aligns with our mission to deliver even more precise diagnostic outcomes.and aslo it's our goal to use more
+information about our patient like gender, historical medical information, or symptoms of disease to have more accurate
+diagnosis and be an assistant for doctor to reduce making mistake in doctor's diagnosis!
 
-    ### Model Performance
-    "We have implemented the VGG16 model as the cornerstone of our diagnostic approach, utilizing its robust capabilities in analyzing medical images.
-    Trained on a dataset comprising 7000 chest X-ray images and 3500 Brain MRI images, our model has demonstrated commendable accuracy in predicting results.
+### More information about model
+User-Friendly Explanation:
+"For those interested in the technical details, the VGG16 model, known for its depth and effectiveness, serves as the backbone of our diagnostic process.
+Its architecture allows for intricate feature extraction, enabling accurate predictions from medical images.
+"""
 
-    ### Ongoing Efforts and Future Goals
-    "As we continually strive for excellence, our commitment to enhancing performance remains unwavering. We envision expanding our dataset in the near future, a crucial step towards refining our model's predictive accuracy.
-    The pursuit of a larger and more diverse dataset aligns with our mission to deliver even more precise diagnostic outcomes.and aslo it's our goal to use more
-    information about our patient like gender, historical medical information, or symptoms of disease to have more accurate
-    diagnosis and be an assistant for doctor to reduce making mistake in doctor's diagnosis!
-
-    ### More information about model
-    User-Friendly Explanation:
-    "For those interested in the technical details, the VGG16 model, known for its depth and effectiveness, serves as the backbone of our diagnostic process.
-    Its architecture allows for intricate feature extraction, enabling accurate predictions from medical images.
-    """
-
-    )
+)
 
 
-    st.image('os.path.join(os.path.dirname(__file__),"..", "pages", "model1.png"', caption='Representation of the model we used')
-
-
-
+st.image(os.path.join(os.path.dirname(__file__),"..", "pages", "model1.png"), caption='Representation of the model we used')
 
 
 
