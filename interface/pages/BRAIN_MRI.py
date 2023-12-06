34891b7f6020b4499e246d4bead3619346516573
Yash Shrivastava <yashs1623@gmail.com>
2023-12-06 10:52:01 +0100

    Add files via upload

--- interface/pages/BRAIN_MRI.py
+++ interface/pages/BRAIN_MRI.py
@@ -1,10 +1,11 @@
 import streamlit as st
+from keras.models import load_model
 from PIL import Image
 import numpy as np
-from util import *
 import requests
-import subprocess
+from util import set_background
 from streamlit_option_menu import option_menu
+import subprocess
 from util import set_background, on_change
 import os
 
@@ -27,13 +28,13 @@ if selected == "Brain":
     #os._exit(0)
 
 # COVID19 Prediction Page
-set_background(os.path.join(os.path.dirname(__file__),"..", "pages", "covid.png"))
+set_background(os.path.join(os.path.dirname(__file__),"..", "pages", "mri_scan.png"))
 
 # set title
-st.markdown("<h1><span style='color: black;'>COVID19 CHEST X-RAY</span> <span style='color: black;'>ANALYSIS</span></h1>", unsafe_allow_html=True)
+st.markdown("<h1><span style='color: black;'>Brain tumor</span> <span style='color: black;'>Detection</span></h1>", unsafe_allow_html=True)
 
 # set header
-st.markdown("<h2 style='color: white;'>Please Upload or Scan An X-Ray Image.</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='color: white;' >Please upload or drop a brain's MRI image.</h2>", unsafe_allow_html=True)
 
 # upload file
 uploaded_file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
@@ -46,17 +47,21 @@ if uploaded_file:
     # Prepare image for API
     image_byte = uploaded_file.read()
 
-    response = requests.post("https://diagnostic-mv6hb5oqca-ew.a.run.app/predict", files={'file': image_byte})
+    response = requests.post("https://diagnosticreportprediction-mv6hb5oqca-ew.a.run.app/predict", files={'file': image_byte})
 
     print("Response Status Code:", response.status_code)
     print("Response Content:", response.content)
 
     if response.status_code == 200:
         result = response.json()
-        if result['Confidence']> 0.60:
-            st.write("<h3 style='color: yellow;'>This Chest X-Ray Shows That The Patient is COVID Positive!</h3>", unsafe_allow_html=True)
-        else:
-             st.write(f"<h3 style='color: yellow;'>This Chest X-Ray Shows That The Patient is COVID Negative!</h3>", unsafe_allow_html=True)
+        if result['Class'] ==  "meningioma_tumor":
+            st.write("<h3 style='color: yellow;'>This Brain MRI Shows Meningioma Tumor!</h3>", unsafe_allow_html=True)
+        elif result['Class'] == "glioma_tumor":
+            st.write(f"<h3 style='color: yellow;'>This Brain MRI Shows Glioma Tumor!</h3>", unsafe_allow_html=True)
+        elif result['Class'] == "no_tumor":
+            st.write(f"<h3 style='color: yellow;'>This Brain MRI Shows No Tumor!</h3>", unsafe_allow_html=True)
+        elif result['Class'] == "pituitary_tumor":
+            st.write(f"<h3 style='color: yellow;'>This Brain MRI Shows Pituitary Tumor!</h3>", unsafe_allow_html=True)
 
     else:
         st.write("<h3 style='color: yellow;'>Error predicting. Please try again.</h3>", unsafe_allow_html=True)
