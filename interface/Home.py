8cae35e54bd0abf0f4f7d5fa24cc56435750d2dc
Yash Shrivastava <yashs1623@gmail.com>
2023-12-06 11:10:10 +0100

    Add files via upload

--- interface/Home.py
+++ interface/Home.py
@@ -1,8 +1,8 @@
 import streamlit as st
+# from streamlit_option_menu import option_menu
 # from keras.models import load_model
 # from PIL import Image
 # import numpy as np
-from streamlit_option_menu import option_menu
 from util import set_background, on_change
 # from streamlit_modal import Modal
 # from streamlit_extras.switch_page_button import switch_page
